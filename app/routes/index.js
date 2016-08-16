d73fc7070d02e0537992042c7cc8686ed8218c99
christine <tacha.christine@gmail.com>
2016-08-16 13:17:59 -0700

    add dynamic route

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,34 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('rental');
-  },
-
-  actions: {
+     return this.store.findAll('rental');
+   },
+   actions: {
     save3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
-      this.transitionTo('index');
-    },
-
-    update(rental, params) {
-      Object.keys(params).forEach(function(key) {
-        if(params[key]!==undefined) {
-          rental.set(key,params[key]);
-        }
-    });
-    rental.save();
-    this.transitionTo('index');
-  },
-    // The following is ^ in plain text:
-    // For each key in the params,
-    // if it is NOT undefined,
-    // take the rental and set the property that matches the current key, to the value of the current key,
-    // after looping through all of the keys, save the rental,
-    // transition to the index route.
-
-    destroyRental(rental) {
-      rental.destroyRecord();
       this.transitionTo('index');
     }
   }
